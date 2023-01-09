54441038a51d37dbdf1285206c8f22dfbd830ef9
AgusIglesias72 <enpalabrass@gmail.com>
2023-01-09 02:24:46 -0300

    change json for fs

--- Api/index.js
+++ Api/index.js
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import data from '../DB/data.json'
+import fs from 'fs'
 import { getOrdersFromGoogle } from '../DB/index.js'
 
 const app = new Hono()
 
 app.use('/*', cors())
+
+const data = fs.readFileSync('./DB/data.json', 'utf8')
 
 app.get('/', (ctx) => {
   return ctx.json([
