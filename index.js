8dc805e15f424496745468b4cc1b857fcf6c1c8e
AgusIglesias72 <enpalabrass@gmail.com>
2023-01-16 23:58:08 -0300

    add Auth header

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 import express from 'express'
 import cors from 'cors'
 import orders from './Api/routes/orders.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { AUTH_TOKEN } = process.env
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 
 const PORT = process.env.PORT ?? 8000
+
+const authentication = (req, res, next) => {
+  const { authorization } = req.headers
+  if (!authorization) {
+    return res.status(401).send({ error: 'No authorization sent' })
+  }
+  const [_, token] = authorization.split(' ')
+  if (token !== AUTH_TOKEN) {
+    return res.status(401).send({ error: 'Unauthorized' })
+  }
+  next()
+}
+
+app.use(authentication)
 
 app.get('/', (_, res) => {
   return res.send({
